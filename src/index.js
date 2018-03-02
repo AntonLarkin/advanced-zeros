2cdc58a5fe73ed49e31a755167947679240e8f0b
Anton <anton11061998@gmail.com>
2018-03-02 11:45:32 +0300

    create project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+var syst = [],temp = 2;
+do {
+    if(base%temp == 0){
+        syst = base;
+        base = Math.round(base/temp);
+    }else {
+        temp++;
+    }
+}
+while(temp <= base)
+
+var rst = 0;
+var a=1;
+while(a<20){
+
+    var num = Math.floor(number/Math.pow(syst, a))
+    rst = rst+ num;
+    a++;
+}
+return rst;
 }
